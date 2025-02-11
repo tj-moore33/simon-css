7041fc753c89b0112af4e89bda7cb2a198c66c00
TJ Moore <moorethomas102@gmail.com>
2025-02-11 11:50:07 -0700

    More jsx changes, header and footer not working

---
+++ src/scores/scores.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Scores(){
+    return (
+        <main calssName='container-fluid bg-secondary text-center'>
+            <div>login displayed here</div>
+             </main>
+    );
+}
