f5dfad2fce71a03422ae92b922db1bb5ab05e82a
TJ Moore <moorethomas102@gmail.com>
2025-02-11 14:08:34 -0700

    Login and Scores HTML and CSS

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,22 @@ import React from "react";
 
 export function Login(){
     return (
-        <main className='container-fluid bg-secondary text-center'>
-            <div>login displayed here</div>
-             </main>
+        <main className="container-fluid bg-secondary text-center">
+        <div>
+          <h1>Welcome to Simon</h1>
+          <form method="get" action="play.html">
+            <div class="input-group mb-3">
+              <span class="input-group-text">@</span>
+              <input class="form-control" type="text" placeholder="your@email.com" />
+            </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text">🔒</span>
+              <input class="form-control" type="password" placeholder="password" />
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+            <button type="submit" class="btn btn-secondary">Create</button>
+          </form>
+        </div>
+      </main>
     );
 }
